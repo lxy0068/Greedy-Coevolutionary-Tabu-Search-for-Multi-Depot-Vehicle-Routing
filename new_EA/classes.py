ed90fcd939ca1d80530c85854ad488bfdd8cb70a
XingyanLiu <124031162+lxy0068@users.noreply.github.com>
2024-12-24 12:02:19 +0800

    Update classes.py

--- new_EA/classes.py
+++ new_EA/classes.py
@@ -1,10 +1,27 @@
 class Vehicle:
+    """
+    Represents a vehicle in the MDVRP (Multi-Depot Vehicle Routing Problem).
+    
+    Attributes:
+        max_duration (float): Maximum duration the vehicle can travel.
+        max_capacity (float): Maximum capacity of the vehicle.
+    """
     def __init__(self, max_duration, max_capacity):
         self.max_duration = max_duration
         self.max_capacity = max_capacity
 
 
 class Customer:
+    """
+    Represents a customer node in the MDVRP.
+
+    Attributes:
+        key (int): Unique identifier for the customer.
+        x (float): X-coordinate of the customer's location.
+        y (float): Y-coordinate of the customer's location.
+        stacking_time (float): Time required to stack/unstack goods for this customer.
+        capacity (float): Goods' demand of the customer.
+    """
     def __init__(self, key, x, y, stacking_time, capacity):
         self.key = key
         self.x = x
@@ -14,6 +31,14 @@ class Customer:
 
 
 class Depot:
+    """
+    Represents a depot in the MDVRP.
+
+    Attributes:
+        key (int): Unique identifier for the depot.
+        x (float): X-coordinate of the depot's location.
+        y (float): Y-coordinate of the depot's location.
+    """
     def __init__(self, key, x, y):
         self.key = key
         self.x = x
@@ -21,6 +46,18 @@ class Depot:
 
 
 class Result:
+    """
+    Represents a solution result for an MDVRP instance.
+    
+    Attributes:
+        generation (int): The generation number of the solution in the coevolutionary algorithm.
+        instance (int): Unique identifier of the instance.
+        capacity (float): Total capacity utilized in this solution.
+        distance (float): Total distance covered in this solution.
+        vehicle (Vehicle): The vehicle used in the solution.
+        depot (Depot): The depot assigned to this solution.
+        customers (list): A list of Customer objects representing the route.
+    """
     def __init__(self, generation, instance):
         self.generation = generation
         self.instance = instance
@@ -32,10 +69,19 @@ class Result:
 
 
 class Fitness:
+    """
+    Represents the fitness of a solution in the MDVRP.
+
+    Attributes:
+        generation (int): The generation number of the solution.
+        instance (int): Unique identifier of the instance.
+        value (float): Fitness value of the solution (e.g., based on total distance, capacity, etc.).
+        phenotype (str): A descriptive string representing the solution's phenotype.
+        best_instance (bool): Flag indicating if this is the best solution for its generation.
+    """
     def __init__(self, generation, instance, value, phenotype):
         self.generation = generation
         self.instance = instance
         self.value = value
         self.phenotype = phenotype
         self.best_instance = False
-
