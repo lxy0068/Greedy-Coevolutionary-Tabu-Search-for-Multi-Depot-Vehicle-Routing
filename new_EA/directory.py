ddc227128994cf7857a89b713afe80706855d710
XingyanLiu <124031162+lxy0068@users.noreply.github.com>
2024-12-24 12:02:58 +0800

    Update directory.py

--- new_EA/directory.py
+++ new_EA/directory.py
@@ -3,30 +3,71 @@ import utils
 import shutil
 
 
-class Directory(object):
+class Directory:
+    """
+    A utility class for managing directory operations related to the evolutionary algorithm.
+
+    Provides methods to create and delete directories for storing images, generations, and instance-specific data.
+    """
+
     @staticmethod
     def create_directories(generation, instance):
+        """
+        Creates the necessary directories for storing images, generation-specific, and instance-specific data.
+
+        Args:
+            generation (str): The generation identifier for which the directory will be created.
+            instance (str): The instance identifier for which the directory will be created.
+        """
         Directory.create_image_dir()
         Directory.create_generation_dir(generation)
         Directory.create_instance_dir(generation, instance)
 
     @staticmethod
     def delete_directories():
+        """
+        Deletes the images directory and its contents if it exists.
+
+        This is used to clean up temporary files or reset the workspace.
+        """
         if os.path.exists(utils.images_dir):
             shutil.rmtree(utils.images_dir)
 
     @staticmethod
     def create_image_dir():
+        """
+        Creates the main directory for storing images if it doesn't already exist.
+
+        The directory path is defined in the `utils.images_dir` variable.
+        """
         if not os.path.exists(utils.images_dir):
             os.makedirs(utils.images_dir)
 
     @staticmethod
     def create_generation_dir(generation):
-        if not os.path.exists(utils.images_dir + utils.generation_dir + generation):
-            os.makedirs(utils.images_dir + utils.generation_dir + generation)
+        """
+        Creates a directory for a specific generation inside the images directory.
+
+        Args:
+            generation (str): The generation identifier for which the directory will be created.
+        
+        The path format is `utils.images_dir + utils.generation_dir + generation`.
+        """
+        generation_path = os.path.join(utils.images_dir, utils.generation_dir, generation)
+        if not os.path.exists(generation_path):
+            os.makedirs(generation_path)
 
     @staticmethod
     def create_instance_dir(generation, instance):
-        if not os.path.exists(utils.images_dir + utils.generation_dir + generation + utils.instance_dir + instance):
-            os.makedirs(utils.images_dir + utils.generation_dir + generation + utils.instance_dir + instance)
+        """
+        Creates a directory for a specific instance within a specific generation directory.
 
+        Args:
+            generation (str): The generation identifier for which the directory will be created.
+            instance (str): The instance identifier for which the directory will be created.
+
+        The path format is `utils.images_dir + utils.generation_dir + generation + utils.instance_dir + instance`.
+        """
+        instance_path = os.path.join(utils.images_dir, utils.generation_dir, generation, utils.instance_dir, instance)
+        if not os.path.exists(instance_path):
+            os.makedirs(instance_path)
