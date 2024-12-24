78f3626e456b510a42159800007cc464b5fffdcd
XingyanLiu <124031162+lxy0068@users.noreply.github.com>
2024-12-24 12:12:21 +0800

    Update image.py

--- new_EA/image.py
+++ new_EA/image.py
@@ -4,9 +4,20 @@ import imageio
 import directory
 
 class Image:
-    full_images = []
+    """
+    The Image class handles image saving, organizing, and generating GIFs for visualizing
+    evolutionary algorithm results, such as best instance animations and route solutions.
+    """
+    full_images = []  # Class-level storage for full images.
 
     def __init__(self, generation, instance):
+        """
+        Initialize the Image object and create necessary directories.
+
+        Args:
+            generation (str): The generation number as a string.
+            instance (str): The instance number as a string.
+        """
         self.generation = generation
         self.instance = instance
         self.image_counter = 1
@@ -14,15 +25,33 @@ class Image:
 
     @staticmethod
     def save_fitness_image(plt):
+        """
+        Save the fitness graph as an image.
+
+        Args:
+            plt (matplotlib.pyplot): The plot object to save.
+        """
         plt.savefig(utils.images_dir + utils.fitness_values_name)
 
     def save(self, plt):
-        plt.savefig(utils.images_dir + utils.generation_dir + self.generation + utils.instance_dir + self.instance +
-                    utils.image_name + str(self.image_counter))
+        """
+        Save a plot image for the current instance and generation.
+
+        Args:
+            plt (matplotlib.pyplot): The plot object to save.
+        """
+        plt.savefig(utils.images_dir + utils.generation_dir + self.generation +
+                    utils.instance_dir + self.instance + utils.image_name + str(self.image_counter))
         self.image_counter += 1
 
     @staticmethod
     def create_best_instances_gif(indexes):
+        """
+        Create a GIF from the images of the best instances.
+
+        Args:
+            indexes (list): List of indexes corresponding to the best instance images.
+        """
         folder_path = utils.images_dir
         images = []
         file_names = Image.full_images
@@ -33,6 +62,9 @@ class Image:
         imageio.mimsave(folder_path + utils.best_instances_gif_name + '.gif', images, duration=1)
 
     def create_instance_gif(self):
+        """
+        Create a GIF for the current instance using saved images.
+        """
         folder_path = utils.images_dir + utils.generation_dir + self.generation + utils.instance_dir + self.instance
         images = []
         file_names = self.get_file_names(folder_path)
@@ -44,6 +76,15 @@ class Image:
         imageio.mimsave(folder_path + '.gif', images, duration=0.5)
 
     def get_file_names(self, folder_path):
+        """
+        Retrieve and sort file names from a directory.
+
+        Args:
+            folder_path (str): The path of the folder to retrieve file names from.
+
+        Returns:
+            list: Sorted list of file names in the folder.
+        """
         file_names = []
         for file in os.listdir(folder_path):
             file_names.append(os.fsdecode(file))
@@ -51,6 +92,14 @@ class Image:
 
     @staticmethod
     def sort_to_order(file_names):
+        """
+        Sort file names in ascending numerical order.
+
+        Args:
+            file_names (list): List of file names to sort.
+
+        Returns:
+            list: Numerically sorted list of file names.
+        """
         file_names.sort(key=lambda i: int(''.join(filter(str.isdigit, i))))
         return file_names
-
